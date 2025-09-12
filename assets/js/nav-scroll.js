c3bce78faeeb107e5ab6603830ca3bef1d018c17
AJ Carpinello <ajcarp81003@gmail.com>
2025-09-12 19:45:05 -0400

    disappearing nav

---
+++ assets/js/nav-scroll.js
@@ -0,0 +1,10 @@
+function scroller () {
+  const nav = document.getElementById("nav");
+  if (window.scrollY > 0) {   // adjust the 100px threshold
+    nav.classList.remove("nav-be-gone");
+  } else {
+    nav.classList.add("nav-be-gone");
+  }
+}
+
+window.addEventListener("scroll", scroller, false);
